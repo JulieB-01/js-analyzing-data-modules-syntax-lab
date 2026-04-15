16aeb5dcf5982650f289e0598d718aca5d6de7b9
JulieB-01 <julietbarasa1@gmail.com>
2026-04-15 22:34:16 +0300

    Modules

--- index.js
+++ index.js
@@ -4,3 +4,16 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+require('datejs')
+function combineUsers(...args){
+
+  const combinedObject = {
+    users: [],
+    merge_date: Date.today().toString("M/d/yyyy")
+  }
+  for (let arr of args){
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+  return combinedObject
+  
+}
